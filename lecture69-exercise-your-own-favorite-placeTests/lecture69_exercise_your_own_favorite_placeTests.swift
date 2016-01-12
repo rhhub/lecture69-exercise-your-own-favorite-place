c148cdef928b252618938bf749f9d33f4803f798
Ryan <ryanhuebert@gmail.com>
2016-01-12 11:11:23 -0800

    Initial Commit

---
+++ lecture69-exercise-your-own-favorite-placeTests/lecture69_exercise_your_own_favorite_placeTests.swift
@@ -0,0 +1,36 @@
+//
+//  lecture69_exercise_your_own_favorite_placeTests.swift
+//  lecture69-exercise-your-own-favorite-placeTests
+//
+//  Created by Ryan Huebert on 1/12/16.
+//  Copyright © 2016 Ryan Huebert. All rights reserved.
+//
+
+import XCTest
+@testable import lecture69_exercise_your_own_favorite_place
+
+class lecture69_exercise_your_own_favorite_placeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
